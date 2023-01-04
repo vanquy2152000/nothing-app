7330924aa2fee51c7870ff3089a88f1585e6f524
Cao Van Quy <quy.187pm20553@vanlanguni.vn>
2023-01-04 08:29:10 +0700

    initial code

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      kanban app
+    </div>
+  );
+}
+
+export default App;
